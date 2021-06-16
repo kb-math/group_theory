8b45e4ed534ec1f28f631185ecf1c7b3fd532ebc
kamil <kamil.bulinski@gmail.com>
2021-06-16 16:35:11 +1000

    WIP

--- group_theory.py
+++ group_theory.py
@@ -30,13 +30,17 @@ class Group(object):
 def generate_finite_semigroup(generators, operation, bad_element_callback = lambda x: False):
     elements = set()
 
-    elements_to_add = set(generators)
+    elements_to_add = set()
+
+    for generator in generators:
+        # want to bail early if we don't like this element
+        if bad_element_callback(generator):
+            return False
+
+        elements_to_add.add(generator)
 
     while elements_to_add:
         for element in elements_to_add:
-            # want to bail early if we don't like this element
-            if bad_element_callback(element):
-                return False
             elements.add(element)
 
         recently_added_elements = elements_to_add
@@ -45,11 +49,14 @@ def generate_finite_semigroup(generators, operation, bad_element_callback = lamb
             for element in recently_added_elements:
                 new_element = operation(generator, element)
                 if new_element not in elements:
+                    # want to bail early if we don't like this element
+                    if bad_element_callback(new_element):
+                        return False
                     elements_to_add.add(new_element)
 
     return elements
 
-# i.e., given operations T, S create the operation Z given by Z(x,y) = (Tx, Sy)
+# given two group operations, create the direct sum
 def product_operation(op1, op2):
     return lambda pair_1, pair_2: (op1(pair_1[0], pair_2[0]), op2(pair_1[1], pair_2[1]))
 
@@ -76,7 +83,6 @@ def test_isomorphism(generators_1, generators_2, op_1, op_2):
 
 
 def line_test_helper(candidate_tuple, x_to_y_map, y_to_x_map):
-    print ("found pair ", candidate_tuple)
     if (candidate_tuple[0] in x_to_y_map) or (candidate_tuple[1] in y_to_x_map):
         return True
 
@@ -133,38 +139,102 @@ def generate_symmetric_group(n):
 
     return generate_permutation_group(generators)
 
+def is_identity_perm(perm):
+    return ( perm == tuple(range(len(perm))) )
+
+def perm_group_is_transitive(group_elements):
+    space = None
+    for group_element in group_elements:
+        if space is None:
+            space = set(range(len(group_element)))
+
+        if group_element[0] in space:
+            space.remove(group_element[0])
+
+        if not space:
+            return True
+
+    return False
 
 class ConjectureTester(object):
     """docstring for ConjectureTester"""
     def __init__(self):
         self.test_subset = set([0,1])
-        self.candidate_actions = {}
+        self.candidate_actions = set()
 
-    def test_subgroup(self, generator_list):
-        test_setwise_stabilizer = lambda g: preserves_subset(lambda g, x: g[x], self.test_subset)
+    def preserves_subset(self, g):
+        return preserves_subset(lambda x: g[x], self.test_subset)
+
+    def test_subgroup_stabilizer(self, generator_list):
+        test_setwise_stabilizer = lambda g: ((not is_identity_perm(g)) and self.preserves_subset(g)) 
         group_elements = generate_finite_semigroup(set(generator_list), multiply_permutations, test_setwise_stabilizer)
 
         if not group_elements:
             return False
 
+        return group_elements
 
+    def check_if_isomorphic_to_existing_subgroup(self, generator_tuple):
+        group_is_new = True
+        new_actions = set()
+        for prev_generator_tuple in self.candidate_actions:
+            result = test_isomorphism(list(generator_tuple), list(prev_generator_tuple), multiply_permutations, multiply_permutations)
+            if result is not False:
+                #we have found an isomorphic copy of a previous group, check if its action is isomorphic
+                group_is_new = False
+                are_isomorphic = self.check_if_action_isomorphic(result)
+                if not are_isomorphic:
+                    new_actions.add(generator_tuple)
 
+        if group_is_new:
+            new_actions.add(generator_tuple)
 
+        self.candidate_actions.update(new_actions)
 
+    def check_if_action_isomorphic(self, result):
+        group_iso_dict = result[0]
+
+        for group_element in group_iso_dict:
+            #the two actions are isomorphic 
+            # checking the stabilizer of 0 is enough
+            if (group_element[0] == 0) != (group_iso_dict[group_element][0] == 0):
+                return False
+
+        return True
 
 
 
 
     def test_conjecture(self, n):
         symmetric_group = generate_symmetric_group(n)
-        symmetric_group_list = list(symmetric_group.group_elements)
 
-        for perm1 in symmetric_group:
-            for perm2 in symmetric_group:
-                self.test_subgroup([perm1, perm2])
+        for perm1 in symmetric_group.group_elements:
+            for perm2 in symmetric_group.group_elements:
+                result = self.test_subgroup_stabilizer([perm1, perm2])
+                if result == False:
+                    continue
+
+                group_elements = result
+
+                self.check_if_isomorphic_to_existing_subgroup((perm1, perm2))
+
+        for action in self.candidate_actions:
+            print (action)
 
 
+def test_transitivity_tester():
+    generators = set()
 
+    generators.add((1,0,2,3,4))
+    generators.add((2,0,1,4,3))
+
+    assert( False == perm_group_is_transitive(generate_finite_semigroup(generators, multiply_permutations)) )
+
+    generators = set()
+
+    generators.add((1,2,3,0))
+
+    assert( True == perm_group_is_transitive(generate_finite_semigroup(generators, multiply_permutations)) )
 
 
 if __name__ == '__main__':
@@ -181,4 +251,11 @@ if __name__ == '__main__':
 
     result = test_isomorphism([(1,2,3,0)], [(3,0,1,2)], multiply_permutations, multiply_permutations)
 
-    print (len(result[0].keys()))
+    for x in result[0]:
+        print (x, result[0][x])
+
+    print("testing transitivity...")
+
+    test_transitivity_tester()
+
+    ConjectureTester().test_conjecture(3)
