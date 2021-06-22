84b6b198b1efca3fb39150aa6f6d8e4594357284
kb-math <kamil.bulinski@gmail.com>
2021-06-22 10:59:39 +1000

    refactor

--- cycle_conjecture.py
+++ cycle_conjecture.py
@@ -4,15 +4,17 @@ class CycleConjectureTester(object):
     def __init__(self,n):
         self.s_n = generate_symmetric_group(n).group_elements
 
-    def test_conjecture(self, k):
+    def test_conjecture_generators(self, gen_tuple_1, gen_tuple_2):
+        if not self.check_conjugate(gen_tuple_1, gen_tuple_2):
+            if self.check_fixed_points_equal(gen_tuple_1, gen_tuple_2):
+                print ("found counterexample!", gen_tuple_1, gen_tuple_2)
+
+    def test_conjecture_all_tuples(self, k):
         generator_tuples = construct_k_distinct_tuples(list(self.s_n), k)
 
         for gen_tuple_1 in generator_tuples:
             for gen_tuple_2 in generator_tuples:
-                if not self.check_conjugate(gen_tuple_1, gen_tuple_2):
-                    if self.check_fixed_points_equal(gen_tuple_1, gen_tuple_2):
-                        print ("found counterexample!", gen_tuple_1, gen_tuple_2)
-                        return
+                self.test_conjecture_generators(gen_tuple_1, gen_tuple_2)
 
         print ("found no counterexample on ", k, "generators")
 
@@ -37,7 +39,7 @@ class CycleConjectureTester(object):
         return (len(conjugator_candidates) > 0)
 
 if __name__ == '__main__':
-    CycleConjectureTester(5).test_conjecture(2)
+    CycleConjectureTester(5).test_conjecture_all_tuples(2)
 
 
 
